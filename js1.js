411dba3eae3f63ce078ee488fcd42997e18371bc
AlenaFox23 <72983824+AlenaFox23@users.noreply.github.com>
2021-10-29 10:43:31 +0000

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,19 @@
+let myButton = document.querySelector('button'); //метод querySelector() возвращает первый элемент (Element) документа, который соответствует указанному селектору или группе селекторов.
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Пожалуйста введите свое имя:');
+    localStorage.setItem('имя', myName);//Объекты веб-хранилища localStorage позволяет хранить значение пары ключ в браузере
+    myHeading.textContent = 'Приветствует тебя, ' + myName + '!!';//textContent – это свойство, которое предназначено для работы с текстовым контентом элемента.
+  }
+
+  if(!localStorage.getItem('имя')) {
+    setUserName();//вызываем тут функцию выше, для того что бы у вас спрашивать имя(если обновить страницу), если вы ранее не ввели ничего
+  } else {
+    var storedName = localStorage.getItem('имя');//вносим ваше имя в переменную
+    myHeading.textContent = 'Приветствует тебя, ' + storedName + '!!';//выводим тут привествие и имя которое ввели
+  }
+
+  myButton.onclick = function() {//функция для того что бы работала кнопка смены имени
+    setUserName();
+  }
