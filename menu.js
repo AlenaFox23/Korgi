6a7c15fa9fb83aaea0e8f6aca993410f91ae0beb
AlenaFox23 <72983824+AlenaFox23@users.noreply.github.com>
2021-11-20 09:02:25 +0000

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,50 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS () ||
+            isMobile.Opera ()||
+            isMobile.Windows ());
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    const iconMenu = document.querySelector('.menu_icon');
+    if (menuIcon) {
+        const menuIcon = document.querySelector('.menu');
+        iconMenu.addEventListener("click", function(e) {
+            iconMenu.classList.toggle('_active');
+        });
+    }
+} else {
+    document.body.classList.add('_pc');
+}
+
+const iconMenu = document.querySelector('.menu_icon');
+    if (menuIcon) {
+        const menuIcon = document.querySelector('.menu');
+        iconMenu.addEventListener("click", function(e) {
+            iconMenu.classList.toggle('_active');
+        });
+
+    }
