4ce7f51e320f6f15ec48cabb925cc4d09b0c6c4d
AlenaFox23 <72983824+AlenaFox23@users.noreply.github.com>
2021-11-21 09:47:59 +0000

    Add files via upload

--- hello.js
+++ hello.js
@@ -4,14 +4,14 @@ let myHeading = document.querySelector('h2');
 function setUserName() {
     var myName = prompt('Пожалуйста введите свое имя:');
     localStorage.setItem('имя', myName);//Объекты веб-хранилища localStorage позволяет хранить значение пары ключ в браузере
-    myHeading.textContent = 'Приветствует тебя, ' + myName + '!!';//textContent – это свойство, которое предназначено для работы с текстовым контентом элемента.
+    myHeading.textContent = 'Приветствует тебя, ' + myName + '!!!';//textContent – это свойство, которое предназначено для работы с текстовым контентом элемента.
   }
 
   if(!localStorage.getItem('имя')) {
     setUserName();//вызываем тут функцию выше, для того что бы у вас спрашивать имя(если обновить страницу), если вы ранее не ввели ничего
   } else {
     var storedName = localStorage.getItem('имя');//вносим ваше имя в переменную
-    myHeading.textContent = 'Приветствует тебя, ' + storedName + '!!';//выводим тут привествие и имя которое ввели
+    myHeading.textContent = 'Приветствует тебя, ' + storedName + '!!!';//выводим тут привествие и имя которое ввели
   }
 
   myButton.onclick = function() {//функция для того что бы работала кнопка смены имени
